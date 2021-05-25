2dbe5630f087a92e7e1be46c5b6416c4947722ef
asbyth <cyronize@gmail.com>
2021-05-25 07:37:27 -0400

    change: update loader & essential

--- src/main/java/club/sk1er/bossbarcustomizer/tweaker/BossbarTweaker.java
+++ src/main/java/club/sk1er/bossbarcustomizer/tweaker/BossbarTweaker.java
@@ -1,10 +1,10 @@
 package club.sk1er.bossbarcustomizer.tweaker;
 
 import club.sk1er.bossbarcustomizer.forge.FMLLoadingPlugin;
-import gg.essential.loader.EssentialTweaker;
+import gg.essential.loader.EssentialSetupTweaker;
 
 @SuppressWarnings("unused")
-public class BossbarTweaker extends EssentialTweaker {
+public class BossbarTweaker extends EssentialSetupTweaker {
     public BossbarTweaker() {
         super(new String[]{FMLLoadingPlugin.class.getName()});
     }
